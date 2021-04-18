9263e108243e75a7e6e0453b8430193b0207d719
Shivam Bale <shivambale98@gmail.com>
2021-04-18 12:50:19 +0530

    animations pending

---
+++ web.js
@@ -0,0 +1,18 @@
+
+// sticky header function
+
+  window.addEventListener("scroll", function () {
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+  });
+
+
+//Menu Toggle
+
+function toggleMenu() {
+  const menuToggle = document.querySelector('.menuToggle');
+  const mobileNavigation = document.querySelector('.navigation');
+  menuToggle.classList.toggle('active');
+  mobileNavigation.classList.toggle('mobile-active');
+}
+
